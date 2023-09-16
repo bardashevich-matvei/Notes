e3312ce8194ac700ac13776ab269d898e4ec66c0
Matvei <bardashevich.matvei@gmail.com>
2023-09-16 05:45:11 +0200

    update user schema

--- notes-backend/src/api/User/schemas/user.schema.ts
+++ notes-backend/src/api/User/schemas/user.schema.ts
@@ -1,20 +1,33 @@
-import { Optional } from '@nestjs/common';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
 
 export type UserDocument = User & Document;
 
 @Schema()
 export class User {
-  @Prop()
+  @Prop({
+    type: String,
+    default: function genUUID() {
+      return uuidv4();
+    },
+  })
+  _id: string;
+
+  @Prop({
+    require: false,
+  })
   name?: string;
 
   @Prop({
-    unique: true
+    unique: true,
+    require: true,
   })
   login: string;
 
-  @Prop()
+  @Prop({
+    require: true,
+  })
   password: string;
 }
 
