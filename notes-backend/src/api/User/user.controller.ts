fa316560913fc1e8212c3d6473bf9760e3276449
Matvei <bardashevich.matvei@gmail.com>
2023-09-16 06:07:11 +0200

    update user controller

--- notes-backend/src/api/User/user.controller.ts
+++ notes-backend/src/api/User/user.controller.ts
@@ -1,6 +1,10 @@
-import { Controller, Get, Req, Post, Body, ClassSerializerInterceptor, UseInterceptors, SerializeOptions } from '@nestjs/common';
+import { Controller, Patch, Get, Post, Body, ClassSerializerInterceptor, UseInterceptors, SerializeOptions, Param, Query, Delete } from '@nestjs/common';
 import { User } from './schemas/user.schema';
 import { UserService } from './user.service';
+import UserResponseDto from '@dto/User/responses/user-response.dto';
+import CreateUserRequestDto from '@dto/User/requests/create-user-request.dto';
+import UpdateUserRequestDto from '@dto/User/requests/update-user-request.dto';
+import { SearchRequest } from '@dto/search/SearchRequest.dto';
 
 @Controller('users')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -12,8 +16,34 @@ export class UserController {
 
 	@Post()
 	async create(
-		@Body() user: User
-	): Promise<User> {
+		@Body() user: CreateUserRequestDto
+	): Promise<UserResponseDto> {
 		return await this.userService.create(user);
+	}
+
+	@Patch(':id')
+	async update(
+		@Body() movie: UpdateUserRequestDto,
+		@Param('id') id: string,
+	): Promise<UserResponseDto> {
+		return this.userService.update(id, movie);
+	}
+
+	@Get()
+	async find(
+		@Query('limit') limit?: number,
+		@Query('offset') offset?: number,
+	): Promise<UserResponseDto[]> {
+		return this.userService.findAll(limit, offset);
+	}
+
+	@Delete(':id')
+	async delete(@Param('id') id: string): Promise<UserResponseDto> {
+		return this.userService.delete(id);
+	}
+  
+	@Post('/search')
+	async search(@Body() selector: SearchRequest): Promise<UserResponseDto[]> {
+		return this.userService.search(selector);
 	}
 }
