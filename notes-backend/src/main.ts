ff6db52bf7b37334f4c6ec2b2b4ae7d279d606f6
Matvei <bardashevich.matvei@gmail.com>
2023-09-16 05:38:06 +0200

    add error handling + server config

--- notes-backend/src/main.ts
+++ notes-backend/src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './utils/error-handler.utils';
+import { MongoExceptionFilter } from './utils/mongo-handler.utils';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: console,
   });
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter(), new MongoExceptionFilter());
+  app.setGlobalPrefix('api');
   // TODO add parsers, logger, cors and api prefix
   await app.listen(3000);
 }
